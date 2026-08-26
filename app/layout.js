1ebcc8f49a1a1e5f2183c792e4a284220871e6ed
padankcool <ngoerahboyz@gmail.com>
2026-08-26 22:12:04 +0800

    bug fixing

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,14 @@
+import "./globals.css";
+
 export const metadata = {
   title: "Unikubali | Etalase Aksesoris Rumah",
   description: "Koleksi aksesoris rumah estetik, mulai dari vas keramik, lampu hias, hingga karpet anyaman berkualitas.",
 };
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="id">
+      <body>{children}</body>
+    </html>
+  );
+}
